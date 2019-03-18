4339d53f56bd9dea12b35be4e2f8671c6c1c13aa
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-03-18 10:20:30 +0700

    :construction: Init main classes

---
+++ lib/gui/mediccare_app.dart
@@ -0,0 +1,15 @@
+///
+/// mediccare_app.dart
+/// Main MedicCare application class
+///
+
+import 'package:flutter/material.dart';
+
+class MedicCareApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // TODO: Implements app
+    );
+  }
+}
