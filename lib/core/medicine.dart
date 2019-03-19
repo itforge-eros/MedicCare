91a35ab719bc5cab2f082bc17f332a572596ff1f
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-03-19 11:32:34 +0700

    :sparkles: Added medicine time logics

--- lib/core/medicine.dart
+++ lib/core/medicine.dart
@@ -14,8 +14,8 @@ class Medicine {
   int _doseAmount;
   int _totalAmount;
   int _remainingAmount;
-  DateTime _dateAdded;
-  List<DateTime> _medicineTime;
+  MedicineTime _medicineTime;
+  final DateTime _dateAdded = DateTime.now();
 
   Medicine({
     String id,
@@ -25,8 +25,7 @@ class Medicine {
     int doseAmount,
     int totalAmount,
     int remainingAmount,
-    DateTime dateAdded,
-    List<DateTime> medicineTime,
+    MedicineTime medicineTime,
   }) {
     this._id = id;
     this._name = name;
@@ -35,7 +34,6 @@ class Medicine {
     this._doseAmount = doseAmount;
     this._totalAmount = totalAmount;
     this._remainingAmount = remainingAmount;
-    this._dateAdded = dateAdded;
     this._medicineTime = medicineTime;
   }
 
@@ -60,17 +58,53 @@ class Medicine {
   int get remainingAmount => this._remainingAmount;
   set remainingAmount(remainingAmount) => this._remainingAmount = remainingAmount;
 
-  DateTime get dateAdded => this._dateAdded;
-  set dateAdded(dateAdded) => this._dateAdded = dateAdded;
-
-  List<DateTime> get medicineTime => this._medicineTime;
+  MedicineTime get medicineTime => this._medicineTime;
   set medicineTime(medicineTime) => this._medicineTime = medicineTime;
 
-  void takeMedicine(int amount) {
-    if (this._remainingAmount - amount < 0) {
+  DateTime get dateAdded => this._dateAdded;
+
+  void takeMedicine() {
+    if (this._remainingAmount - this._doseAmount < 0) {
       throw OutOfMedicineException();
     } else {
-      this._remainingAmount -= amount;
+      this._remainingAmount -= this._doseAmount;
     }
   }
+}
+
+class MedicineTime {
+  bool _breakfast;
+  bool _lunch;
+  bool _dinner;
+  bool _night;
+  bool _beforeMeal;
+
+  MedicineTime({
+    bool breakfast = false,
+    bool lunch = false,
+    bool dinner = false,
+    bool night = false,
+    bool beforeMeal = false,
+  }) {
+    this._breakfast = breakfast;
+    this._lunch = lunch;
+    this._dinner = dinner;
+    this._night = night;
+    this._beforeMeal = beforeMeal;
+  }
+
+  bool get breakfast => this._breakfast;
+  set breakfast(breakfast) => this._breakfast = breakfast;
+
+  bool get lunch => this._lunch;
+  set lunch(lunch) => this._lunch = lunch;
+
+  bool get dinner => this._dinner;
+  set dinner(dinner) => this._dinner = dinner;
+
+  bool get night => this._night;
+  set night(night) => this._night = night;
+
+  bool get beforeMeal => this._beforeMeal;
+  set beforeMeal(beforeMeal) => this._beforeMeal = beforeMeal;
 }
--- lib/core/user.dart
+++ lib/core/user.dart
@@ -21,6 +21,7 @@ class User {
   List<Medicine> _medicineList;
   List<Doctor> _doctorList;
   List<Appointment> _appointmentList;
+  UserSettings _userSettings;
 
   User({
     String id,
@@ -35,6 +36,7 @@ class User {
     List<Medicine> medicineList,
     List<Doctor> doctorList,
     List<Appointment> appointmentList,
+    UserSettings userSettings,
   }) {
     this._id = id;
     this._email = email;
@@ -48,6 +50,7 @@ class User {
     this._medicineList = medicineList;
     this._doctorList = doctorList;
     this._appointmentList = appointmentList;
+    this._userSettings = userSettings;
   }
 
   String get id => this._id;
@@ -85,4 +88,123 @@ class User {
 
   List<Appointment> get appointmentList => this._appointmentList;
   set appointmentList(appointmentList) => this._appointmentList = appointmentList;
+
+  List<DateTime> getMedicineTime(Medicine medicine) {
+    List<DateTime> medicineTime = List<DateTime>();
+    DateTime firstTime;
+    int times = medicine.totalAmount ~/ medicine.doseAmount;
+
+    DateTime breakfastDateTime = DateTime(
+      medicine.dateAdded.year,
+      medicine.dateAdded.month,
+      medicine.dateAdded.day,
+      this._userSettings.breakfastTime.inHours,
+      this._userSettings.breakfastTime.inMinutes % 60,
+    );
+
+    DateTime lunchDateTime = DateTime(
+      medicine.dateAdded.year,
+      medicine.dateAdded.month,
+      medicine.dateAdded.day,
+      this._userSettings.lunchTime.inHours,
+      this._userSettings.lunchTime.inMinutes % 60,
+    );
+
+    DateTime dinnerDateTime = DateTime(
+      medicine.dateAdded.year,
+      medicine.dateAdded.month,
+      medicine.dateAdded.day,
+      this._userSettings.dinnerTime.inHours,
+      this._userSettings.dinnerTime.inMinutes % 60,
+    );
+
+    DateTime nightDateTime = DateTime(
+      medicine.dateAdded.year,
+      medicine.dateAdded.month,
+      medicine.dateAdded.day,
+      this._userSettings.nightTime.inHours,
+      this._userSettings.nightTime.inMinutes % 60,
+    );
+
+    if (medicine.dateAdded.compareTo(breakfastDateTime) < 0) {
+      firstTime = breakfastDateTime;
+    } else if (medicine.dateAdded.compareTo(lunchDateTime) < 0) {
+      firstTime = lunchDateTime;
+    } else if (medicine.dateAdded.compareTo(dinnerDateTime) < 0) {
+      firstTime = dinnerDateTime;
+    } else if (medicine.dateAdded.compareTo(nightDateTime) < 0) {
+      firstTime = nightDateTime;
+    } else {
+      firstTime = breakfastDateTime.add(Duration(days: 1));
+    }
+
+    List<Duration> durations = <Duration>[
+      this._userSettings.lunchTime - this._userSettings.breakfastTime,
+      this._userSettings.dinnerTime - this._userSettings.lunchTime,
+      this._userSettings.nightTime - this._userSettings.dinnerTime,
+      Duration(days: 1) - this._userSettings.nightTime + this._userSettings.breakfastTime,
+    ];
+
+    for (int i = 0; i < times; i++) {
+      medicineTime.add(firstTime);
+      firstTime.add(durations[i % 4]);
+    }
+
+    return medicineTime;
+  }
+}
+
+class UserSettings {
+  static const Duration _defaultBreakfastTime = Duration(
+    hours: 7,
+    minutes: 0,
+  );
+  static const Duration _defaultLunchTime = Duration(
+    hours: 12,
+    minutes: 0,
+  );
+  static const Duration _defaultDinnerTime = Duration(
+    hours: 18,
+    minutes: 0,
+  );
+  static const Duration _defaultNightTime = Duration(
+    hours: 22,
+    minutes: 0,
+  );
+
+  Duration _breakfastTime;
+  Duration _lunchTime;
+  Duration _dinnerTime;
+  Duration _nightTime;
+
+  UserSettings({
+    Duration breakfastTime,
+    Duration lunchTime,
+    Duration dinnerTime,
+    Duration nightTime,
+  }) {
+    this._breakfastTime = breakfastTime;
+    this._lunchTime = lunchTime;
+    this._dinnerTime = dinnerTime;
+    this._nightTime = nightTime;
+  }
+
+  Duration get breakfastTime => this._breakfastTime;
+  set breakfastTime(breakfastTime) => this._breakfastTime = breakfastTime;
+
+  Duration get lunchTime => this._lunchTime;
+  set lunchTime(lunchTime) => this._lunchTime = lunchTime;
+
+  Duration get dinnerTime => this._dinnerTime;
+  set dinnerTime(dinnerTime) => this._dinnerTime = dinnerTime;
+
+  Duration get nightTime => this._nightTime;
+  set nightTime(nightTime) => this._nightTime = nightTime;
+
+  void resetDefault() {
+    this._breakfastTime = UserSettings._defaultBreakfastTime;
+    this._lunchTime = UserSettings._defaultLunchTime;
+    this._dinnerTime = UserSettings._defaultDinnerTime;
+    this._nightTime = UserSettings._defaultNightTime;
+  }
 }
