bdb65a85f4c0fa4becbcd56bcda60e7700be09cb
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-05-17 15:21:46 +0700

    :sparkles: Added login validators and alerts

--- lib/gui/login_page.dart
+++ lib/gui/login_page.dart
@@ -6,7 +6,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mediccare/util/alert.dart';
+import 'package:mediccare/util/firestore_utils.dart';
+import 'package:mediccare/util/validator.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,10 +19,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   static final TextEditingController _controllerEmail = TextEditingController();
   static final TextEditingController _controllerPassword = TextEditingController();
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   void initState() {
@@ -34,6 +37,7 @@ class _LoginPageState extends State<LoginPage> {
   void signInWithEmail() async {
     FirebaseUser user;
     this._trimEmailField();
+
     try {
       user = await _auth.signInWithEmailAndPassword(
           email: _controllerEmail.text, password: _controllerPassword.text);
@@ -80,6 +84,11 @@ class _LoginPageState extends State<LoginPage> {
         icon: Icon(Icons.mail),
         hintText: 'Email Address',
       ),
+      validator: (String email) {
+        if (!Validator.isEmail(email)) {
+          return 'Please enter a valid email address';
+        }
+      },
     );
 
     final TextFormField textFormFieldPassword = TextFormField(
@@ -90,6 +99,11 @@ class _LoginPageState extends State<LoginPage> {
         icon: Icon(Icons.lock),
         hintText: 'Password',
       ),
+      validator: (String password) {
+        if (password.isEmpty) {
+          return 'Please enter password';
+        }
+      },
     );
 
     final RaisedButton buttonLogin = RaisedButton(
@@ -98,7 +112,9 @@ class _LoginPageState extends State<LoginPage> {
       color: Theme.of(context).primaryColor,
       textColor: Colors.white,
       onPressed: () {
-        signInWithEmail();
+        if (_formKey.currentState.validate()) {
+          signInWithEmail();
+        }
       },
     );
 
@@ -111,27 +127,30 @@ class _LoginPageState extends State<LoginPage> {
       },
     );
 
-    return Scaffold(
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 30.0, right: 30.0),
-          children: <Widget>[
-            Container(
-              child: Image.asset('assets/logo_temporary.jpg'),
-              padding: EdgeInsets.only(left: 30.0, right: 30.0),
-            ),
-            SizedBox(height: 20.0),
-            textFormFieldEmail,
-            SizedBox(height: 10.0),
-            textFormFieldPassword,
-            SizedBox(height: 20.0),
-            buttonLogin,
-            Container(
-              child: buttonRegister,
-              alignment: Alignment.centerRight,
-            )
-          ],
+    return Form(
+      key: this._formKey,
+      child: Scaffold(
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            children: <Widget>[
+              Container(
+                child: Image.asset('assets/logo_temporary.jpg'),
+                padding: EdgeInsets.only(left: 30.0, right: 30.0),
+              ),
+              SizedBox(height: 20.0),
+              textFormFieldEmail,
+              SizedBox(height: 10.0),
+              textFormFieldPassword,
+              SizedBox(height: 20.0),
+              buttonLogin,
+              Container(
+                child: buttonRegister,
+                alignment: Alignment.centerRight,
+              )
+            ],
+          ),
         ),
       ),
     );
