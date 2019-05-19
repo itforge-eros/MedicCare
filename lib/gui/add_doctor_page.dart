be68b267ad6da4a78a280ead20c4e0e81ef194a4
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-05-19 11:13:05 +0700

    :sparkles: Completes Add Doctor Page

--- lib/gui/add_doctor_page.dart
+++ lib/gui/add_doctor_page.dart
@@ -20,6 +20,14 @@ class AddDoctorPage extends StatefulWidget {
 }
 
 class _AddDoctorPageState extends State<AddDoctorPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  static final TextEditingController _controllerPrefix = TextEditingController();
+  static final TextEditingController _controllerFirstName = TextEditingController();
+  static final TextEditingController _controllerLastName = TextEditingController();
+  static final TextEditingController _controllerWard = TextEditingController();
+  static final TextEditingController _controllerHospital = TextEditingController();
+  static final TextEditingController _controllerPhone = TextEditingController();
+  static final TextEditingController _controllerNotes = TextEditingController();
   File _image;
 
   Future getImage() async {
@@ -42,47 +50,79 @@ class _AddDoctorPageState extends State<AddDoctorPage> {
         backgroundColor: Colors.white.withOpacity(0.9),
         elevation: 0.1,
       ),
-      body: Center(
-        child: ListView(
-          padding: EdgeInsets.only(left: 30.0, top: 15.0, right: 30.0, bottom: 15.0),
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: getImage,
-              tooltip: 'Pick Image',
-              child: Icon(Icons.add_a_photo),
-            ),
-            // TODO: Completes form
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Prefix'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Firstname'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Lastname'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Ward'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Hospital'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Phone'),
-            ),
-            TextFormField(
-              maxLines: 4,
-              decoration: InputDecoration(hintText: 'Notes'),
-            ),
-            SizedBox(height: 20.0),
-            RaisedButton(
-              child: Text('Save'),
-              onPressed: () {
-                widget._refreshState();
-                Navigator.pop(context);
-              },
-            ),
-          ],
+      body: Form(
+        key: this._formKey,
+        child: Center(
+          child: ListView(
+            padding: EdgeInsets.only(left: 30.0, top: 15.0, right: 30.0, bottom: 15.0),
+            children: <Widget>[
+              FloatingActionButton(
+                onPressed: getImage,
+                tooltip: 'Pick Image',
+                child: Icon(Icons.add_a_photo),
+              ),
+              TextFormField(
+                controller: _controllerPrefix,
+                decoration: InputDecoration(hintText: 'Prefix'),
+                validator: (String text) {
+                  if (text.isEmpty) {
+                    return 'Please fill doctor\'s prefix';
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _controllerFirstName,
+                decoration: InputDecoration(hintText: 'First name'),
+                validator: (String text) {
+                  if (text.isEmpty) {
+                    return 'Please fill doctor\'s first name';
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _controllerLastName,
+                decoration: InputDecoration(hintText: 'Last name'),
+                validator: (String text) {
+                  if (text.isEmpty) {
+                    return 'Please fill doctor\'s last name';
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _controllerWard,
+                decoration: InputDecoration(hintText: 'Ward'),
+              ),
+              TextFormField(
+                controller: _controllerHospital,
+                decoration: InputDecoration(hintText: 'Hospital'),
+                validator: (String text) {
+                  if (text.isEmpty) {
+                    return 'Please fill hospital';
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _controllerPhone,
+                decoration: InputDecoration(hintText: 'Phone'),
+              ),
+              TextFormField(
+                controller: _controllerNotes,
+                maxLines: 4,
+                decoration: InputDecoration(hintText: 'Notes'),
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                child: Text('Save'),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  if (this._formKey.currentState.validate()) {
+                    widget._refreshState();
+                    Navigator.pop(context);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
