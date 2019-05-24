fab97d21901bb725628e5b8c5ddeac02ca910db5
Supakit Theanthunyakit <mr.pokiloli@gmail.com>
2019-05-24 17:38:14 +0700

    Add Map

--- lib/gui/homepage.dart
+++ lib/gui/homepage.dart
@@ -12,6 +12,7 @@ import 'package:mediccare/core/medicine_schedule.dart';
 import 'package:mediccare/core/user.dart';
 import 'package:mediccare/core/user_setting.dart';
 import 'package:mediccare/gui/add_medicine_page.dart';
+import 'package:mediccare/gui/map_page.dart';
 import 'package:mediccare/gui/profile_page.dart';
 import 'package:mediccare/gui/add_appointment_page.dart';
 import 'package:mediccare/gui/medicine_page.dart';
@@ -566,7 +567,7 @@ class _HomepageState extends State<Homepage> {
       leftAppointment(),
       overview(),
       rightDoctor(),
-      Center(child: Text('Waiting for map API implementation.')),
+      MapPage(),
     ];
 
     List headerTitle = [
--- lib/gui/medic_care_app.dart
+++ lib/gui/medic_care_app.dart
@@ -8,6 +8,7 @@ import 'package:mediccare/gui/intro_page.dart';
 import 'package:mediccare/gui/login_page.dart';
 import 'package:mediccare/gui/register_page.dart';
 import 'package:mediccare/gui/homepage.dart';
+import 'package:mediccare/gui/map_page.dart';
 import 'package:mediccare/gui/user_settings_page.dart';
 
 class MedicCareApp extends StatelessWidget {
