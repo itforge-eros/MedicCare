9abfedfdc64f925bdc71d86256a30daf8cf2b716
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-05-27 11:08:56 +0700

    :art: Removed debug banner

--- lib/gui/medic_care_app.dart
+++ lib/gui/medic_care_app.dart
@@ -15,6 +15,7 @@ class MedicCareApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
